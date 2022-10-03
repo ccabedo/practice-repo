6a0473f8e5f00540c0e5347f2bd0d370a5fde321
Harrison <harriwilson5534@gmail.com>
2022-10-03 17:23:31 -0400

    Here you go

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-//OOGA BOOGA
+// OOGA BOOGA
+
+// Sometimes, I think that I would like to own a monkey. It would be cute, and fun,
+// and I could teach it to play video games.
+
+// But then i remember that story of the monkey that ripped off that woman's face,
+// and I think "yeah probably shouldn't do that"
