71f2bbd3a186b19d310a8e33531ae15a7f4da8b5
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-10-03 17:30:41 -0400

    team rockets blasting off again

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // OOGA BOOGA
 
 // Sometimes, I think that I would like to own a monkey. It would be cute, and fun,
+    // POKEMON
+    // TEAM ROCKET MOTTO HERE
 // and I could teach it to play video games.
 
 // But then i remember that story of the monkey that ripped off that woman's face,
